1cf14132d11379cc6dcaa7cdcc573a8fface88dc
Stefan Lenders <stefan@lenders-it.nl>
2025-07-20 22:22:01 +0200

    Start

--- app/app/Models/User.php
+++ app/app/Models/User.php
@@ -3,17 +3,15 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
-use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Support\Str;
-use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
-    use HasApiTokens, HasFactory, Notifiable, hasUuids;
+    use HasFactory, Notifiable;
 
     /**
      * The attributes that are mass assignable.
--- app/routes/web.php
+++ app/routes/web.php
@@ -1,35 +1,22 @@
 <?php
 
-use App\Http\Controllers\Api\BackupController;
 use Illuminate\Support\Facades\Route;
 use Livewire\Volt\Volt;
 
-use App\Livewire\BackupServers\Index as BackupServersIndex;
-use App\Livewire\BackupServers\Create as BackupServersCreate;
-use App\Livewire\BackupServers\Edit as BackupServersEdit;
+Route::get('/', function () {
+    return view('welcome');
+})->name('home');
 
-
+Route::view('dashboard', 'dashboard')
+    ->middleware(['auth', 'verified'])
+    ->name('dashboard');
 
 Route::middleware(['auth'])->group(function () {
-    Route::get('/', function () {
-        return view('dashboard');
-    })->name('home');
     Route::redirect('settings', 'settings/profile');
 
     Volt::route('settings/profile', 'settings.profile')->name('settings.profile');
     Volt::route('settings/password', 'settings.password')->name('settings.password');
     Volt::route('settings/appearance', 'settings.appearance')->name('settings.appearance');
-
-    Route::get('/backup-servers', BackupServersIndex::class)->name('backup-servers.index');
-    Route::get('/backup-servers/create', BackupServersCreate::class)->name('backup-servers.create');
-    Route::get('/backup-servers/{backupServer}/edit', BackupServersEdit::class)->name('backup-servers.edit');
 });
 
 require __DIR__.'/auth.php';
-
-
-Route::apiResource('api/backup-result', BackupController::class);
-
-//Route::middleware(['api', 'auth:sanctum'])->group(function () {
-//
-//});
