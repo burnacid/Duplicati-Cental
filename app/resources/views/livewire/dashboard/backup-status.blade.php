6c7add81fca7499b534e50ed05060fe8421c6b4e
Stefan Lenders <stefan@lenders-it.nl>
2025-08-08 15:58:27 +0200

    Improve dashboard

--- app/resources/views/livewire/dashboard/backup-status.blade.php
+++ app/resources/views/livewire/dashboard/backup-status.blade.php
@@ -144,8 +144,10 @@
             @endforelse
         </div>
 
-        <div class="mt-4">
-            {{ $latestBackupResults->links() }}
+        <div class="mt-4 text-center">
+            <a href="{{ route('backup-results') }}" class="inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                View All Backup Results
+            </a>
         </div>
     </div>
 </div>
